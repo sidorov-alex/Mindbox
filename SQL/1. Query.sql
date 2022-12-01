2f85d62a16552bd2aa1cafd8c861c7c11e6cb70d
sidorov-alex <trycatch@fastmail.com>
2022-12-01 23:55:55 +1000

    Добавлен SQL-запрос

---
+++ SQL/1. Query.sql
@@ -0,0 +1,10 @@
+﻿SELECT	Product.Name
+	,	ISNULL(Category.Name, '')
+	FROM
+		Product
+	LEFT JOIN
+		ProductCategory ON Product.Id = ProductId
+	LEFT JOIN
+		Category ON CategoryId = Category.Id
+    ORDER BY
+		Product.Name;
---
+++ SQL/2. Create DB.sql
@@ -0,0 +1,38 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(255) NOT NULL
+);
+
+CREATE UNIQUE INDEX [IX_Product_Name] ON [dbo].[Product] ([Name]);
+
+CREATE TABLE [dbo].[Category]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(255) NOT NULL
+);
+
+CREATE UNIQUE INDEX [IX_Category_Name] ON [dbo].[Category] ([Name]);
+
+CREATE TABLE [dbo].[ProductCategory]
+(
+	[ProductId] INT NOT NULL , 
+    [CategoryId] INT NOT NULL, 
+    PRIMARY KEY ([CategoryId], [ProductId]), 
+    CONSTRAINT [FK_ProductCategory_Product] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id]), 
+    CONSTRAINT [FK_ProductCategory_Category] FOREIGN KEY ([CategoryId]) REFERENCES [Category]([Id])
+);
+
+INSERT INTO Product (Id, Name) VALUES
+    (1, 'Tea')
+,   (2, 'Milk')
+,   (3, 'Bread');
+
+INSERT INTO Category (Id, Name) VALUES
+    (1, 'Beverages')
+,   (2, 'Milk Products');
+
+INSERT INTO ProductCategory (ProductId, CategoryId) VALUES
+    (1, 1)
+,   (2, 1)
+,   (2, 2);
